fd97feaeae4ade4bf2bcd272e3b23e519d9849e2
shadow3aaa <shadow3aaa@qq.com>
2024-07-10 20:04:35 +0800

    asfd

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/bash
 repo init -u https://android.googlesource.com/platform/manifest -b master-ndk --partial-clone
 repo sync -c -j 16
+
+for dir in prebuilts/clang/host/*; do
+    local here=$(realpath .)
+    cd dir
+    git checkout main
+    git pull
+    cd $here
+done
+
 cd ndk
 poetry env use ../prebuilts/python/linux-x86/bin/python3
 poetry install
+python checkbuild.py
