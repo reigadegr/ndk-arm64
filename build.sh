9e2b9694a2fda9f10d74b7e63fdeb4bae9cdba57
shadow3aaa <shadow3aaa@qq.com>
2024-07-10 19:35:00 +0800

    asf

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 repo init -u https://android.googlesource.com/platform/manifest -b master-ndk --partial-clone
-repo sync
+repo sync -c -j 8
+poetry env use ../prebuilts/python/linux-x86/bin/python3
+poetry install
