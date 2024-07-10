41b81fa4710fe288d686410cbea407982f95d754
shadow3aaa <shadow3aaa@qq.com>
2024-07-10 19:39:06 +0800

    asdf

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 repo init -u https://android.googlesource.com/platform/manifest -b master-ndk --partial-clone
-repo sync -c -j 8
+repo sync -c -j 16
+cd ndk
 poetry env use ../prebuilts/python/linux-x86/bin/python3
 poetry install
